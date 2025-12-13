1261c59f87f03a1f94e68935ec86a8987294f8b6
panchaminandu-png <panchami.nandu@gmail.com>
2025-12-13 12:13:02 +0530

    Create health.js

---
+++ backend/api-gateway/src/routes/health.js
@@ -0,0 +1,7 @@
+import express from "express";
+import { healthCheck } from "../controllers/healthController.js";
+
+const router = express.Router();
+router.get("/", healthCheck);
+
+export default router;
