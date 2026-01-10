275d63d1c6b048888e8a219eee56c3a14e58d1df
panchaminandu-png <panchami.nandu@gmail.com>
2026-01-10 18:35:58 +0530

    Create ResultsView.styles.ts

---
+++ mobile/components/ResultsView/mobile/components/ResultsView/mobile/components/ResultsView/ResultsView.styles.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    padding: 10
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: "600"
+  }
+});
